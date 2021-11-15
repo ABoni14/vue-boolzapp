0a83c2d3164e899f52a2625ad4b32f4d981d28d2
ABoni14 <aboni150@gmail.com>
2021-11-15 14:46:18 +0100

    MILLESTON 1, last commit

--- js/script.js
+++ js/script.js
@@ -35,12 +35,12 @@ const app = new Vue({
         messages: 
         [
           {
-            data: "10/01/2020 15:30:55",
+            data: "10/01/2020 17:30:35",
             text: "Oggi puoi uscire? Andiamo al cinema?",
             stato: "inviato"
           },
           {
-            data: "10/01/2020 16:15:22",
+            data: "10/01/2020 17:39:22",
             text: "Mi piacerebbe ma devo andare dal dottore!",
             stato: "ricevuto"
           },
@@ -53,12 +53,12 @@ const app = new Vue({
         messages: 
         [
           {
-            data: "10/01/2020 15:30:55",
+            data: "10/01/2020 11:30:55",
             text: "Ciao Samuele, l'ultima volta hai dimenticato da me gli occhiali",
             stato: "inviato"
           },
           {
-            data: "10/01/2020 16:15:22",
+            data: "10/01/2020 12:01:22",
             text: "Oh scusa! Mi sono proprio dimenticato, vengo oggi pomeriggio a prenderli, grazie!",
             stato: "ricevuto"
           },
@@ -71,12 +71,12 @@ const app = new Vue({
         messages: 
         [
           {
-            data: "10/01/2020 15:30:55",
+            data: "10/01/2020 21:56:55",
             text: "Oggi puoi uscire? Andiamo al cinema?",
             stato: "inviato"
           },
           {
-            data: "10/01/2020 16:15:22",
+            data: "10/01/2020 22:15:09",
             text: "Mi piacerebbe ma devo andare dal dottore!",
             stato: "ricevuto"
           },
