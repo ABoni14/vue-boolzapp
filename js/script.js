e0a8a334c2878179995587ee43767e5537a9b2e7
ABoni14 <aboni150@gmail.com>
2021-11-15 14:03:57 +0100

    struttura html, css, aggiunta dei contatti con js

---
+++ js/script.js
@@ -0,0 +1,87 @@
+const app = new Vue({
+  el: "#app",
+
+  data: {
+
+    contacts: 
+    [
+      {
+        name: "Michele",
+        img: "img/avatar_1.jpg",
+        visible: true,
+        messages: 
+        [
+          {
+            data: "10/01/2020 15:30:55",
+            text: "Hai portato a spasso il cane?",
+            stato: "inviato"
+          },
+          {
+            data: "10/01/2020 15:50:00",
+            text: "Ricordati di dargli da mangiare",
+            stato: "inviato"
+          },
+          {
+            data: "10/01/2020 16:15:22",
+            text: "Tutto fatto!",
+            stato: "ricevuto"
+          },
+        ] 
+      },
+      {
+        name: "Fabio",
+        img: "img/avatar_2.jpg",
+        visible: true,
+        messages: 
+        [
+          {
+            data: "10/01/2020 15:30:55",
+            text: "Oggi puoi uscire? Andiamo al cinema?",
+            stato: "inviato"
+          },
+          {
+            data: "10/01/2020 16:15:22",
+            text: "Mi piacerebbe ma devo andare dal dottore!",
+            stato: "ricevuto"
+          },
+        ] 
+      },
+      {
+        name: "Samuele",
+        img: "img/avatar_3.jpg",
+        visible: true,
+        messages: 
+        [
+          {
+            data: "10/01/2020 15:30:55",
+            text: "Ciao Samuele, l'ultima volta hai dimenticato da me gli occhiali",
+            stato: "inviato"
+          },
+          {
+            data: "10/01/2020 16:15:22",
+            text: "Oh scusa! Mi sono proprio dimenticato, vengo oggi pomeriggio a prenderli, grazie!",
+            stato: "ricevuto"
+          },
+        ] 
+      },
+      {
+        name: "Luisa",
+        img: "img/avatar_6.jpg",
+        visible: true,
+        messages: 
+        [
+          {
+            data: "10/01/2020 15:30:55",
+            text: "Oggi puoi uscire? Andiamo al cinema?",
+            stato: "inviato"
+          },
+          {
+            data: "10/01/2020 16:15:22",
+            text: "Mi piacerebbe ma devo andare dal dottore!",
+            stato: "ricevuto"
+          },
+        ] 
+      },
+    ]
+  }
+})
